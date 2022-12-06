f8e6696d131583593eb06169a616dde7d2c1b778
Alexander Medved <medved1991alexander@gmail.com>
2022-12-06 20:59:20 +0300

    Add Base models for Catalog

---
+++ Models/CatalogItem.cs
@@ -0,0 +1,21 @@
+﻿namespace MyShop.Models
+{
+    public sealed class CatalogItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public CatalogType? CatalogType { get; set; }   
+        public CatalogBrand? CatalogBrand { get; set; }
+
+        public CatalogItem(string name, string discription, string pictireUrl, decimal price)
+        {
+            Name = name;
+            Description = discription;
+            PictureUrl = pictireUrl;
+            Price = price;
+        }  
+    }
+}
