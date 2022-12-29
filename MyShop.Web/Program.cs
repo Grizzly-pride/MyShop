b16c59865a9830babe376eb45b6946d34a03afe3
Alexander Medved <medved1991alexander@gmail.com>
2022-12-30 00:17:22 +0300

    add auto database update

--- MyShop.Web/Program.cs
+++ MyShop.Web/Program.cs
@@ -3,13 +3,17 @@ using MyShop.ApplicationCore.Entities;
 using MyShop.Services;
 using MyShop.Infrastructure;
 using MyShop.Web.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Hosting;
+using MyShop.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-#region Ioc
+#region IoC
 builder.Services.AddCoreServices();
 builder.Services.AddScoped(typeof(IRepository<CatalogItem>), typeof(LocalCatalofItemRepository));
 builder.Services.AddScoped<ICatalogItemViewModelService, CatalogItemViewModelService>();
@@ -17,6 +21,28 @@ Dependencies.ConfigureServices(builder.Configuration, builder.Services);
 #endregion
 
 var app = builder.Build();
+
+app.Logger.LogInformation("Database migration running...");
+
+#region Auto Database Update
+using (var scope = app.Services.CreateScope())
+{
+    var scopedProvider = scope.ServiceProvider;
+    try
+    {
+        var catalogContext = scopedProvider.GetRequiredService<CatalogContext>();
+        if (catalogContext.Database.IsSqlServer())
+        {
+            catalogContext.Database.Migrate();
+        }
+        //await CatalogContextSeed.SeedAsync(catalogContext, app.Logger);
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "An error occurred seeding the DB.");
+    }
+}
+#endregion
 
 app.Logger.LogInformation("App Created...");
 
@@ -26,7 +52,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+}   
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
