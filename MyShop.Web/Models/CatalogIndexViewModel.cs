3487414a935a75a4f68c5454db45eb757bed96e7
Alex_demo <medved1991alexander@gmail.com>
2023-01-05 14:15:18 +0300

    Add CatalogIndexViewModel

---
+++ MyShop.Web/Models/CatalogIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MyShop.Models;
+
+namespace MyShop.Web.Models
+{
+	internal sealed class CatalogIndexViewModel
+	{
+		public List<CatalogItemViewModel>? CatalogItems { get; set; }
+		public List<SelectListItem>? Brands { get; set; }
+		public List<SelectListItem>? Types { get; set; }
+		public int? BrandFilterApplied { get; set; }
+		public int? TypeFilterApplied { get; set; }
+	}
+}
